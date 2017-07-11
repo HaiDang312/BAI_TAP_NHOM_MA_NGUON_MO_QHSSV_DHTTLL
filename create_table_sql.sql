0c96b862d847f4e427e058afe61e1839709c6626
buithetuan <buithetuan1@gmail.com>
2017-07-11 09:47:41 +0700

    code tao bang khen thuong va ki luat

---
+++ create_table_sql.sql
@@ -0,0 +1,36 @@
+﻿create table KHENTHUONG
+(
+
+	MAKT varchar(8) primary key not null,
+	TENKT nvarchar(8) not null,
+	GHICHU nvarchar(100) not null,
+
+)
+
+create table QTKHENTHUONG
+(
+
+	MASV	varchar(13) FOREIGN KEY REFERENCES SINHVIEN(MASV) on delete cascade on update cascade  NOT NULL ,
+	MAKT varchar(8)  FOREIGN KEY REFERENCES KHENTHUONG(MAKT) on delete cascade on update cascade  NOT NULL ,
+	NGAYKT date not null,
+	constraint PK2 primary key (MASV, MAKT, NGAYKT),
+)
+
+create table KYLUAT
+(
+
+	MAKL varchar(8) primary key not null,
+	TENKL nvarchar(8) not null,
+	GHICHU nvarchar(100) not null,
+
+)
+
+create table QTKYLUAT
+(
+
+	MASV	varchar(13) FOREIGN KEY REFERENCES SINHVIEN(MASV) on delete cascade on update cascade  NOT NULL ,
+	MAKL varchar(8)  FOREIGN KEY REFERENCES KYLUAT(MAKL) on delete cascade on update cascade  NOT NULL ,
+	NGAYKL date not null,
+	NGAYHH date not null,
+	constraint PK3 primary key (MASV, MAKL, NGAYKL),
+)
